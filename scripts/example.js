d12cb4bd8a5bec781c77f3f4cf89aa4c501cdd80
Luke Forsythe <luke@lukef.com>
2016-07-08 17:00:17 +0100

    Worked out a better way of getting the first letter from a string

--- scripts/example.js
+++ scripts/example.js
@@ -1,21 +1,54 @@
 module.exports = function(robot) {
 
-  robot.hear(/hello/i, function(msg) {
+// Stores words begining with letters
+
+var letterA = ["apple", "ass", "Adderall"];
+var letterB = ["bike", "baby squid", "big zombie"];
+var letterC = ["cunt", "cat", "crapper"];
+
+// res.match[1] stores any word included in string by user
+
+robot.hear( /spell (.*)/i, function( res ) {
+    var word;
+    word = res.match[1];
+
+    // Checks word begins with certain letter
+
+    if ( word.startsWith("a") )
+      return res.reply ( "Did you mean \'"+letterA[Math.floor(Math.random()*letterA.length)]+"\', Dave?" );
+
+    if ( word.charAt(0) === "b" )
+      return res.reply ( "Did you mean \'"+letterB[Math.floor(Math.random()*letterB.length)]+"\', Dave?" );
+
+    if ( word.charAt(0) === "c" )
+      return res.reply ( "Did you mean \'"+letterC[Math.floor(Math.random()*letterC.length)]+"\', Dave?" );
+
+    else
+      return res.reply ("I don't understand what you mean, Dave.");
+
+    });
+
+// Standard simple listeners and replies
+
+robot.hear( /hello/i, function( msg ) {
     return msg.send("Hello, Dave.");
     });
 
-  robot.hear(/thanks/i, function(msg) {
-    return msg.send("You are welcome, Dave.");
+robot.hear( /thanks/i, function( msg ) {
+    return msg.send ( "You are welcome, Dave." );
     });
 
-  robot.hear(/spell (.*)/i, function(res) {
-    var word;
-    word = res.match[1];
-    if (word.charAt("C"))
-    return res.reply("Did you mean \'Chicken\', Dave?");
-    else
-    return res.reply("I have no idea what you mean, Dave.");
-    });
+
+  };
+
+
+
+
+
+
+// ---------------------------
+// ********** SHITE ************
+// ---------------------------
 
     // robot.respond(/open the (.*) doors/i, function(res) {
     //   var doorType;
@@ -26,9 +59,6 @@ module.exports = function(robot) {
     //   return res.reply("Opening " + doorType + " doors");
     //   }
     // });
-
-  };
-
 
     // function(msg) {
     //   return msg.send("Did you mean \'Chicken\', Dave?");
